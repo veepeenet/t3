2796436c47523c365b411dfa21f96c793c25edc0
veepee <veepeenet@outlook.com>
2022-11-16 10:35:12 -0600

    end of 15

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path")
 
-const postCSSPlugins = [require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+const postCSSPlugins = [
+  require("postcss-simple-vars"), 
+  require("postcss-nested"), 
+  require("autoprefixer")
+]
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
